cf127f37b47d5e7b9a3cc531031c61dc7ff8e0c2
Jo-HyeRin <sayhyelin12@naver.com>
2022-09-21 17:44:56 +0900

    초기 설정, index 작동 확인

---
+++ src/main/java/site/metacoding/TemplateEngine/TemplateEngineApplication.java
@@ -0,0 +1,13 @@
+package site.metacoding.TemplateEngine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemplateEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemplateEngineApplication.class, args);
+	}
+
+}
